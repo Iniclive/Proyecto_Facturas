e81e66646aeff699be7088e3ad4cec02f3edadb8
Iniclive <iniclive@gmail.com>
2026-04-07 15:00:29 +0200

    Comienzo a crear el ENDPOINT de usuarios, pendiente de agregar porst/put

---
+++ FacturacionAPI/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using inercya.EntityLite;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Facturas.Data;
+
+namespace FacturacionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly FacturacionDataService _dataService;
+        public UsersController(FacturacionDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>>GetAllUsers()
+        {
+            if (!User.IsInRole("admin"))
+            {
+                return Forbid();
+            }
+            var users = await _dataService.UserRepository
+                .Query(UserProjections.BaseTable)
+                .ToListAsync();       
+            return Ok(users);
+        }
+
+    }
+}
